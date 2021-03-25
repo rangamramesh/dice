e27479779c788921e0aad74d7f070972f2c917a5
Ramesh Rangam <rangamramesh036@gmail.com>
2021-03-25 15:01:45 +0530

    added environments

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.example.dice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.dice.databinding.ActivityMainBinding
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val dataBind=DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
-        val viewModel = ViewModelProvider(this,ViewModelFactory()).get(MainViewModel::class.java)
+        AppCenter.start(
+            application, "eb5d8e2a-bb4e-43ce-bbf7-a42486207ab5",
+            Analytics::class.java, Crashes::class.java
+        )
+        val dataBind = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        )
+        val viewModel = ViewModelProvider(this, ViewModelFactory()).get(MainViewModel::class.java)
         dataBind.viewModel = viewModel
-        dataBind.lifecycleOwner =this
+        dataBind.lifecycleOwner = this
 
     }
 }
